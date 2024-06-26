2ce553870fd6a30843fe6d8f9dee4dfed4e6c816
abadredd <49213550+abadredd@users.noreply.github.com>
2024-06-25 21:14:02 -0400

    Update route_planner.h

--- src/route_planner.h
+++ src/route_planner.h
@@ -16,10 +16,12 @@ class RoutePlanner {
 
     // The following methods have been made public so we can test them individually.
     void AddNeighbors(RouteModel::Node *current_node);
+
     float CalculateHValue(RouteModel::Node const *node);
     std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
     RouteModel::Node *NextNode();
-
+	bool Compare(const RouteModel::Node &a, const RouteModel::Node &b);
+  
   private:
     // Add private variables or methods declarations here.
     std::vector<RouteModel::Node*> open_list;
